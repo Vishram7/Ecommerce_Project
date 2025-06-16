8245a30b5eaa724298630c6da95660aa6cf017af
Vishram7 <rvishram17@gmail.com>
2025-06-16 15:57:29 +0530

    demo commit 2

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,7 +12,7 @@ router.post('/register', async (req, res) => {
     try {
         const newUser = new User({ email, password })
         await newUser.save()
-        return res.status(201).json({ message: 'User registered ' })
+        return res.status(201).json({ message: 'User registered successfully' })
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
